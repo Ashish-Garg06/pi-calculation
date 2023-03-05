25736b6310611d768b25ce6ddae7ef0f691c2c93
Ashish Garg <ashishgarg5006@gmail.com>
2023-03-05 13:46:12 +0530

    added comments

--- picalculation.py
+++ picalculation.py
@@ -7,13 +7,16 @@ from time  import time
 
 
 class PiChudnovsky:
+    # Constants used in the algorithm
     A = 13591409
     B = 545140134
     C = 640320
     D = 426880
     E = 10005
     C3_24  = C ** 3 // 24
+    # Number of digits per term in the series
     DIGITS_PER_TERM = math.log(53360 ** 3) / math.log(10)  #=> 14.181647462725476
+    # Name of the file to write the computed value of pi
     FILENAME = "pi.txt"
 
     def __init__(self, digits):
@@ -21,19 +24,25 @@ class PiChudnovsky:
         :param int digits: digits of PI computation
         """
         self.digits = digits
+        # Number of terms required for desired digits of pi
         self.n      = math.floor(self.digits / self.DIGITS_PER_TERM + 1)
+        # Precision of the computation based on number of digits
         self.prec   = math.floor((self.digits + 1) * math.log2(10))
 
     def compute(self):
         """ Computation """
         try:
-            tm_s = time()
+            tm_s = time() # Start timer
+            # Compute P, Q and T using binary splitting algorithm
             p, q, t = self.__bsa(0, self.n)
+            # Compute the square root of (E * 10^(2*digits))
             one_sq = mpz(10) ** (2 * self.digits)
             sqrt_c = isqrt(self.E * one_sq)
+            # Compute pi using the formula and write it to the file
             pi = (q * self.D * sqrt_c) // t
             with open(self.FILENAME, "w") as f:
                 f.write(str(pi))
+            # Return elapsed time
             return time() - tm_s
         except Exception as e:
             raise
@@ -49,12 +58,15 @@ class PiChudnovsky:
                 if a == 0:
                     p_ab = q_ab = mpz(1)
                 else:
+                    # Compute P and Q for the current value of a
                     p_ab = mpz((6 * a -5) * (2 * a - 1) * (6 * a - 1))
                     q_ab = mpz(a * a * a * self.C3_24)
+                # Compute T for the current value of a
                 t_ab = p_ab * (self.A + self.B * a)
                 if a & 1:
                     t_ab *= -1
             else:
+                # Divide the interval [a,b] into two halves and compute P, Q and T for each half recursively
                 m = (a + b) // 2
                 p_am, q_am, t_am = self.__bsa(a, m)
                 p_mb, q_mb, t_mb = self.__bsa(m, b)
